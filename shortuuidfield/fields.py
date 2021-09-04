c99de3839cd43848ed320a7578a2c3238f93683a
nick-fournier <45876721+nick-fournier@users.noreply.github.com>
2021-09-03 18:13:26 -0700

    Update fields.py
    Added prefix suffix options

--- shortuuidfield/fields.py
+++ shortuuidfield/fields.py
@@ -4,7 +4,7 @@ import sys
 from django.db.models import CharField
 
 
-class ShortUUIDField(CharField):
+class CustomShortUUIDField(CharField):
     """
     A field which stores a Short UUID value in base57 format. This may also have
     the Boolean attribute 'auto' which will set the value on initial save to a
@@ -15,34 +15,44 @@ class ShortUUIDField(CharField):
     def __init__(self, auto=True, *args, **kwargs):
         self.auto = auto
         # We store UUIDs in base57 format, which is fixed at 22 characters.
-        kwargs['max_length'] = 22
+        if kwargs['prefix']:
+            self.prefix = kwargs['prefix']
+        else:
+            self.prefix = ''
+            
+        if kwargs['suffix']):            
+            self.suffix = kwargs['suffix']
+        else:
+            self.suffix = ''
+        
+        kwargs['max_length'] = 22 + len(self.prefix) + len(self.suffix)
         if auto:
             # Do not let the user edit UUIDs if they are auto-assigned.
             kwargs['editable'] = False
             kwargs['blank'] = True
            # kwargs['unique'] = True  # if you want to be paranoid, set unique=True in your instantiation of the field.
 
-        super(ShortUUIDField, self).__init__(*args, **kwargs)
+        super(CustomShortUUIDField, self).__init__(*args, **kwargs)
 
     def pre_save(self, model_instance, add):
         """
         This is used to ensure that we auto-set values if required.
         See CharField.pre_save
         """
-        value = super(ShortUUIDField, self).pre_save(model_instance, add)
+        value = super(CustomShortUUIDField, self).pre_save(model_instance, add)
         if self.auto and not value:
             # Assign a new value for this attribute if required.
-            value = six.text_type(shortuuid.uuid())
+            value = six.text_type(self.prefix + shortuuid.uuid() + self.suffix)
             setattr(model_instance, self.attname, value)
         return value
 
     def formfield(self, **kwargs):
         if self.auto:
             return None
-        return super(ShortUUIDField, self).formfield(**kwargs)
+        return super(CustomShortUUIDField, self).formfield(**kwargs)
 
 try:
     from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], [r"^shortuuidfield\.fields\.ShortUUIDField"])
+    add_introspection_rules([], [r"^shortuuidfield\.fields\.CustomShortUUIDField"])
 except ImportError:
     pass
